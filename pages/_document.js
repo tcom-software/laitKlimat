5982986a99e0446eaeea3035c22fc73b8fdaad80
Gogi Loloyan <fiftyone157@gmail.com>
2020-10-09 17:31:09 +0300

    remover stylesheet

--- pages/_document.js
+++ pages/_document.js
@@ -31,7 +31,7 @@ export default class MyDocument extends Document {
         styles: (
           <>
             {initialProps.styles}
-            {sheet.getStyleElement()}
+            {/* {sheet.getStyleElement()} */}
           </>
         ),
       };
